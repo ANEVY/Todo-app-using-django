0785f8ce1b1a983b89af0f6dc93ce95f010332bb
Akombo Neville Akwo <akomboneville55@gmail.com>
2024-08-16 15:54:14 +0100

    Created Task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Task(models.Model):
+    TASK_STATUS = [
+        ("PENDING", "Pending"),
+        ("COMPLETE", "Complete")
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    due_date = models.DateField()
+    status = models.CharField(choices=TASK_STATUS, default="PENDING")
+
+    def __str__(self):
+        return self.title
 
 # Create your models here.
